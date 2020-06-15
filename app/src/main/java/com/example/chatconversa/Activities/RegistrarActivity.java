0ca140677239fd23ebd1f286b42beeb955bbc5d0
Tamara <tvale.sv@gmail.com>
2020-06-15 03:39:20 -0400

    Layout registrar

--- app/src/main/java/com/example/chatconversa/Activities/RegistrarActivity.java
+++ app/src/main/java/com/example/chatconversa/Activities/RegistrarActivity.java
@@ -6,11 +6,11 @@ import android.os.Bundle;
 
 import com.example.chatconversa.R;
 
-public class MainActivity extends AppCompatActivity {
+public class RegistrarActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_registrar);
     }
 }
